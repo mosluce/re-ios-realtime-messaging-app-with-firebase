995e3165f03847d6362c8cfdec2b084dd5345c99
mosluce <mosluce@gmail.com>
2017-06-02 13:42:58 +0800

    fix(content): fix ch4.1

--- LetsChat/ChatViewController.swift
+++ LetsChat/ChatViewController.swift
@@ -18,7 +18,7 @@ class ChatViewController: JSQMessagesViewController {
     // 房間名稱
     var roomName: String!
     // 訊息資料
-    var messages = [[String: String]]()
+    var messages = [[String: Any]]()
     // 訊息記錄節點
     var messageRef: FIRDatabaseReference!
 
@@ -26,8 +26,43 @@ class ChatViewController: JSQMessagesViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        edgesForExtendedLayout = []
         
         messageRef = FIRDatabase.database().reference().child("messages/\(roomID!)")
+        messageRef.observeSingleEvent(of: .value, with: {[unowned self] (snapshot) in
+            
+            // messages 架構為
+            // messageId1
+            // |- messageData1
+            // messageId2
+            // |- messageData2
+            guard let messagesCollection = snapshot.value as? [String: [String: Any]] else {
+                return
+            }
+            
+            messagesCollection.forEach({ (msgId, msgData) in
+                var message = msgData
+                message["id"] = msgId
+                self.messages.append(message)
+            })
+            
+            self.collectionView?.reloadData()
+            self.messageRef.observe(.childAdded, with: {[weak self] (snapshot) in
+                guard let msgData = snapshot.value as? [String: Any], let senderId = msgData["senderId"] as? String else {
+                    return
+                }
+                
+                if senderId == self?.senderId() {
+                    return
+                }
+                
+                var message = msgData
+                message["id"] = snapshot.key
+                
+                self?.messages.append(message)
+                self?.finishReceivingMessage()
+            })
+        })
     }
 
     override func didReceiveMemoryWarning() {
@@ -46,13 +81,17 @@ class ChatViewController: JSQMessagesViewController {
     override func didPressSend(_ button: UIButton, withMessageText text: String, senderId: String, senderDisplayName: String, date: Date) {
         // 實作傳送訊息
         let messageNode = messageRef.childByAutoId()
-        
-        messageNode.setValue([
+        let message = [
             "senderId": senderId,
             "senderDisplayName": senderDisplayName,
-            "date": date.timeIntervalSince1970 * 1000
-        ]) {[unowned self] (error, _) in
-            self.finishSendingMessage()
+            "date": date.timeIntervalSince1970,
+            "text": text
+        ] as [String : Any];
+        
+        self.messages.append(message)
+        
+        messageNode.setValue(message) {[weak self] (error, _) in
+            self?.finishSendingMessage()
         }
     }
 
@@ -66,4 +105,32 @@ class ChatViewController: JSQMessagesViewController {
     }
     */
 
+}
+
+extension ChatViewController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView, messageDataForItemAt indexPath: IndexPath) -> JSQMessageData {
+        let message = messages[indexPath.item]
+        
+        guard let text = message["text"] as? String,
+            let senderId = message["senderId"] as? String,
+            let senderDisplayName = message["senderDisplayName"] as? String,
+            let interval = message["date"] as? TimeInterval
+            else { fatalError("data format exception") }
+        
+        let date = Date(timeIntervalSince1970: interval)
+        
+        return JSQMessage(senderId: senderId, senderDisplayName: senderDisplayName, date: date, text: text)
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView, messageBubbleImageDataForItemAt indexPath: IndexPath) -> JSQMessageBubbleImageDataSource? {
+        return JSQMessagesBubbleImageFactory().outgoingMessagesBubbleImage(with: UIColor.green)
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView, avatarImageDataForItemAt indexPath: IndexPath) -> JSQMessageAvatarImageDataSource? {
+        return nil
+    }
 }
